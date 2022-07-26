44169b5ae2954af44d0b14c2ca8bddfbdfbb37a7
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-07-26 13:43:17 +0200

    it5  logo Cards and backgroundColor loaded dynamically

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import visa from '../assets/images/visa.png';
+import masterCard from '../assets/images/master-card.svg';
 
 
 export default function creditCard(props) {
     const { type, number, expirationMonth, expirationYear, bank, owner, bgColor, color } = props
     
+    const logoCard = type === 'Visa' ? visa : masterCard;
+
+    const divStyle = {
+      backgroundColor: `${bgColor}`,
+      color: `${color}`,
+    };
+
   return (
 
-    <div className='eachCreditCard'>
+    <div className='eachCreditCard' style={divStyle}>
+
+        
+
+        <div>
+          <img src={logoCard} alt="Credit Card Logo"/>
+        </div>
+
         <div>{number}</div>
         <div>Expires {expirationMonth}/{expirationYear}</div>
         <div>{bank}</div>
